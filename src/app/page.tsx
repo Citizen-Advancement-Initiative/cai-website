94cade42b2e3a1107da12a68d3280dce5b17ba20
Alan Amalemba <alanlsamalemba@gmail.com>
2025-09-05 22:39:37 +0300

    feat: add header and navigation bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,56 @@
+import caiWebsiteLogo from "@/assets/cai-website-logo.png";
+import heroImage from "@/assets/hero-image.jpeg";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
-      <h1>Citizen Advancement Initiative</h1>
+      <header className="fixed inset-x-0 top-0 flex items-center justify-between gap-6 px-6 py-6 mx-2 my-2 rounded-full backdrop-blur-md bg-black/50">
+        <Image
+          width={262}
+          height={49}
+          src={caiWebsiteLogo}
+          alt="Website logo"
+        />
+
+        <nav className="text-white">
+          <ul className="flex gap-6 text-lg ">
+            <li>
+              <p>What we do</p>
+            </li>
+            <li>
+              <p>About us</p>
+            </li>
+            <li>
+              <p>Where we work</p>
+            </li>
+            <li>
+              <p>Our publications</p>
+            </li>
+            <li>
+              <p>Our projects</p>
+            </li>
+            <li>
+              <p>Gallery</p>
+            </li>
+            <li>
+              <p>Get involved</p>
+            </li>
+           
+          </ul>
+        </nav>
+
+        <div>
+          <button className="px-6 py-3 text-lg border rounded-full border-white/80 text-white/80">
+            Support us
+          </button>
+        </div>
+      </header>
+
+      <main
+        style={{ backgroundImage: `url(${heroImage.src})` }}
+        className="w-full h-screen bg-bottom bg-cover"
+      ></main>
     </>
   );
 }
